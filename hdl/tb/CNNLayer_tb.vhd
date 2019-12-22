1c63f8bb595ae3b251ea9dd17186058390872dbb
Filippo Scotto <filipscotto@gmail.com>
2019-12-22 15:56:03 +0100

    tests with memory and reset

--- hdl/tb/CNNLayer_tb.vhd
+++ hdl/tb/CNNLayer_tb.vhd
@@ -9,14 +9,14 @@ end cnnlayer_tb;
 
 architecture bhv of cnnlayer_tb is
   constant T_CLK        : time    :=  8 ns;
-  constant T_RESET	: time    := 25 ns;
+  constant T_RESET	: time    := 10 ns;
 
   constant INPUT_WIDTH  : natural := 5;
   constant INPUT_HEIGHT : natural := 5;
 
   -- Testbench signals
   signal clk_tb         : std_logic := '0';
-  signal reset_tb       : std_logic := '0'; 
+  signal reset_tb       : std_logic := '1'; 
   signal end_sim        : std_logic := '1';
 
   signal Cin : cnn_matrix_t(0 to INPUT_WIDTH-1, 0 to INPUT_HEIGHT-1) := (
@@ -26,8 +26,6 @@ architecture bhv of cnnlayer_tb is
     ("00000000", "00000000", "00000001", "00000001", "00000000"),
     ("00000000", "00000001", "00000001", "00000000", "00000000")
   );
-
-  signal cout_i : cnn_cell_t := (others => '0');
   
   -- Component to test (DUT) declaration
   component CNNLayer is
@@ -38,22 +36,20 @@ architecture bhv of cnnlayer_tb is
       reset : in std_logic;
 
       -- just a cell as output to test it...
-      Cin   : in  cnn_matrix_t(0 to InputWidth-1, 0 to InputHeight-1);
-      out_i : out cnn_cell_t
+      Cin   : in  cnn_matrix_t(0 to InputWidth-1, 0 to InputHeight-1)
     );
   end component;
 
 begin
   clk_tb <= (not(clk_tb) and end_sim) after T_CLK / 2;
-  -- reset_tb <= '0' after T_RESET;
+  reset_tb <= '0' after T_RESET;
 
   test_CNN: CNNLayer
   generic map (InputWidth => INPUT_WIDTH, InputHeight => INPUT_HEIGHT)
   port map(
     clk   => clk_tb,      
     reset => reset_tb,
-    Cin   => Cin,
-    out_i => cout_i
+    Cin   => Cin
   );
 		
   cnnlayer_process: process(clk_tb, reset_tb)
