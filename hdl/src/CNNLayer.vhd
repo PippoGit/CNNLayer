10d3ad391d6cab59ae8c47e3bdb717cf27328c24
Filippo Scotto <filipscotto@gmail.com>
2020-01-26 17:38:14 +0100

    more upd

--- hdl/src/CNNLayer.vhd
+++ hdl/src/CNNLayer.vhd
@@ -25,7 +25,49 @@ architecture CNNLayer_Arch of CNNLayer is
 
   -- type cnn_memory_t is array(0 to CNN_MEMORY_SIZE-1) of cnn_cell_t; 
   signal CNNMemory : cnn_matrix_t(0 to FilterWidth-1, 0 to FilterHeight-1);
+ 
+  signal cin_reg : cnn_matrix_t(0 to InputWidth-1, 0 to InputHeight-1);
+  signal flt_reg : cnn_matrix_t(0 to FilterWidth-1, 0 to FilterHeight-1);
+
+  -- Register
+  component CNNRegister is
+    generic(CNNWidth:natural; CNNHeight:natural);
+    port (
+      clk   : in  std_logic;
+      reset : in  std_logic;
+      d_in  : in  cnn_matrix_t(0 to CNNWidth-1, 0 to CNNHeight-1);
+      d_out : out cnn_matrix_t(0 to CNNWidth-1, 0 to CNNHeight-1)
+    );
+  end component;
 begin
+
+  -- Registers
+  CinRegister : CNNRegister
+    generic map(CNNWidth => InputWidth, CNNHeight => InputHeight)
+    port map(
+      clk   => clk,
+      reset => reset,
+      d_in  => Cin,
+      d_out => cin_reg
+    );
+
+  FltRegister : CNNRegister
+    generic map(CNNWidth => FilterWidth, CNNHeight => FilterHeight)
+    port map(
+      clk   => clk,
+      reset => reset,
+      d_in  => flt,
+      d_out => flt_reg
+    );
+
+  CoutRegister : CNNRegister
+    generic map(CNNWidth => FilterWidth, CNNHeight => FilterHeight)
+    port map(
+      clk   => clk,
+      reset => reset,
+      d_in  => CNNMemory,
+      d_out => Cout
+    );
 
   -- Process
   process(clk, reset)
@@ -47,7 +89,7 @@ begin
          end if;
 
          -- convolution for i-th output
-         CNNMemory(index_r, index_c)   <= slice(Cin, index_r, index_c, FilterWidth, FilterHeight)*flt;
+         CNNMemory(index_r, index_c)   <= slice(cin_reg, index_r, index_c, FilterWidth, FilterHeight)*flt_reg;
 
          -- increment indices...
          index_c   := index_c + 1;
@@ -57,5 +99,5 @@ begin
   end process;
   
   -- Output 
-  Cout <= CNNMemory;
+  -- Cout <= CNNMemory;
 end CNNLayer_Arch;
--- hdl/src/CNNRegister.vhd
+++ hdl/src/CNNRegister.vhd
@@ -6,14 +6,15 @@ library work;
 use work.cnn_types.all;
 
 entity CNNRegister is
+  generic (CNNWidth:natural; CNNHeight:natural);
   port (
     -- Clock and reset
     clk   : in std_logic;
     reset : in std_logic;
 
     -- In/Out
-    d_in  : in  cnn_cell_t;
-    d_out : out cnn_cell_t
+    d_in  : in  cnn_matrix_t(0 to CNNWidth-1, 0 to CNNHeight-1);
+    d_out : out cnn_matrix_t(0 to CNNWidth-1, 0 to CNNHeight-1)
   );
 end CNNRegister;
 
@@ -22,7 +23,7 @@ begin
   process(clk, reset)
   begin
     if reset = '1' then
-       d_out <= (others => '0');
+       d_out <= (others => (others => "00000000")); -- reset the register
     elsif (clk='1' and clk'event) then
        d_out <= d_in;
     end if;
