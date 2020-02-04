28024b603546286f77efa4ca403d37d84da7d498
Filippo Scotto <filipscotto@gmail.com>
2020-02-04 23:22:20 +0100

    Update and rename template.txt to template.vhd

--- script/template.vhd
+++ script/template.vhd
@@ -18,21 +18,19 @@ architecture bhv of cnnlayer_tb is
   constant FILTER_HEIGHT  : natural := {filter_height};
 
   constant ADDRESS_LENGTH : natural :=  {address_length};
-  constant CNN_OUTPUT_BIT : natural :=  {cnn_output_bit}
+  constant CNN_OUTPUT_BIT : natural :=  {cnn_output_bit};
 
   -- Testbench signals
   signal clk_tb         : std_logic := '0';
   signal reset_tb       : std_logic := '0'; 
   signal end_sim        : std_logic := '1';
 
-  signal Cin : cnn_matrix_t(0 to {input_width}-1, 0 to {input_height}-1) := {cin_matrix};
+  signal Cin : cnn_matrix_t(0 to INPUT_WIDTH-1, 0 to INPUT_HEIGHT-1) := {cin_matrix};
+  signal flt : cnn_matrix_t(0 to FILTER_WIDTH-1, 0 to FILTER_HEIGHT-1) := {flt_matrix};
 
-  signal flt : cnn_matrix_t(0 to {filter_width}-1, 0 to {filter_height}-1) := {flt_matrix};
-
-  signal mem_data_out_s : std_logic_vector({cnn_output_bit}
--1 downto 0);
+  signal mem_data_out_s : std_logic_vector(CNN_OUTPUT_BIT-1 downto 0);
   signal mem_rd_en_s    : std_logic;
-  signal mem_rd_addr_s  : std_logic_vector({address_length}-1 downto 0);
+  signal mem_rd_addr_s  : std_logic_vector(ADDRESS_LENGTH-1 downto 0);
 
   
   component CNNLayer is
@@ -57,10 +55,11 @@ begin
   reset_tb <= '1' after T_RESET;
 
   test_CNN: CNNLayer
-  generic map (InputWidth => {input_width}, InputHeight => {input_height}, 
-               FilterWidth => {filter_width}, FilterHeight => {filter_height},
-               AddressLength => {address_length}, NumBitData => {cnn_output_bit}
-)
+  generic map (
+    InputWidth => INPUT_WIDTH, InputHeight => INPUT_HEIGHT, 
+    FilterWidth => FILTER_WIDTH, FilterHeight => FILTER_HEIGHT,
+    AddressLength => ADDRESS_LENGTH, NumBitData => CNN_OUTPUT_BIT
+  )
   port map(
     clk   => clk_tb,      
     reset => reset_tb,
@@ -78,21 +77,13 @@ begin
     if(reset_tb = '0') then
       t := 0;
       mem_rd_en_s    <= '0';
-      mem_rd_addr_s  <= "ZZZZ";
+      mem_rd_addr_s  <= (others => '0');
     elsif(clk_tb='1' and clk_tb'event) then
       case(t) is
-        -- testing read from memory (10 clock cycles are enough for 5x5 + 3x3)
-        when 10 => mem_rd_en_s   <= '1'; mem_rd_addr_s <= "0000";
-        when 11 => mem_rd_addr_s <= "0001";
-        when 12 => mem_rd_addr_s <= "0010";
-        when 13 => mem_rd_addr_s <= "0011";
-        when 14 => mem_rd_addr_s <= "0100";
-        when 15 => mem_rd_addr_s <= "0101";
-        when 16 => mem_rd_addr_s <= "0110";
-        when 17 => mem_rd_addr_s <= "0111";
-        when 18 => mem_rd_addr_s <= "1000";
-        when 19 => mem_rd_en_s   <= '0'; 
-        when 30 => end_sim       <= '1';
+        -- testing read from memory
+        when {start_simulation_time} => mem_rd_en_s   <= '1'; mem_rd_addr_s <= (others => '0');
+{simulation_cases}
+        when {end_simulation_time} => end_sim <= '1'; mem_rd_en_s <= '0';
         when others => null;
       end case;
       t := t + 1;
