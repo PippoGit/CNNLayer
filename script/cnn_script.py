57f772ce0c52460596106d8ebbd101267d173da9
Filippo Scotto <filipscotto@gmail.com>
2020-02-05 11:19:15 +0100

    Update cnn_script.py

--- script/cnn_script.py
+++ script/cnn_script.py
@@ -4,16 +4,36 @@ import math
 
 # constants
 NUM_BIT_INTEGER = 8
+MAX_NUMBER      = 2**(NUM_BIT_INTEGER-1) - 1 
 CLOCK_PERIOD    = '8 ns'
 RESET_TIME      = '12 ns'
 
 
 def vhdl_matrix_literal(matrix):
     vhdl_mat = '('
-    for i in range(0, matrix.shape[1]):
-        vhdl_mat += '(' + ', '.join([('"' + np.binary_repr(val, NUM_BIT_INTEGER) + '"') for val in matrix[i]]) + ')' + (', ' if i < matrix.shape[1]-1 else '')
+    for i in range(0, matrix.shape[0]):
+        vhdl_mat += '(' + ', '.join([('"' + np.binary_repr(val, NUM_BIT_INTEGER) + '"') for val in matrix[i]]) + ')' + (', ' if i < matrix.shape[0]-1 else '')
     vhdl_mat += ')'
     return vhdl_mat
+
+
+def commented_matrix(matrix):
+    result = ''
+    for i in range(0, matrix.shape[0]):
+        result += '--     ' + '    '.join([('"' + str(int(val)) + '"') for val in matrix[i]]) +  '\n'
+    return result
+
+
+def conv(cin, flt):
+    hs = cin.shape[1] - flt.shape[1] + 1
+    vs = cin.shape[0] - flt.shape[0] + 1
+    result = np.zeros((vs, hs))
+
+    for i in range(vs):
+        for j in range(hs):
+            result[i][j] = (flt.flatten()*cin[i:i+flt.shape[0], j:j+flt.shape[1]].flatten()).sum()
+
+    return result            
 
 
 def zip_parameters(cin, flt, addr_len, output_bit, start_simulation_time, end_simulation_time, simulation_cases):
@@ -30,7 +50,10 @@ def zip_parameters(cin, flt, addr_len, output_bit, start_simulation_time, end_si
         'simulation_cases'      : simulation_cases,
         'cin_matrix'            : vhdl_matrix_literal(cin),
         'flt_matrix'            : vhdl_matrix_literal(flt),
-        'cnn_output_bit'        : str(output_bit)
+        'cnn_output_bit'        : str(output_bit),
+        'cin_matrix_numeric'    : commented_matrix(cin),
+        'flt_matrix_numeric'    : commented_matrix(flt),
+        'res_matrix_numeric'    : commented_matrix(conv(cin, flt))
     }
     return param
 
@@ -60,22 +83,28 @@ def generate_testbench(cin, flt):
     return
 
 
+def generate_random_testbench(cin_shape, flt_shape):
+    generate_testbench(cin=np.random.randint(MAX_NUMBER+1, size=cin_shape),
+                       flt=np.random.randint(MAX_NUMBER+1, size=flt_shape))
+    return
+
 
 def main():
     cin = np.array([
-        [1, 2, 3, 4, 5],
-        [1, 2, 3, 4, 5],
-        [1, 2, 3, 4, 5],
-        [1, 2, 3, 4, 5],
-        [1, 2, 3, 4, 5]
+        [1, 1, 1, 0, 0],
+        [0, 1, 1, 1, 0],
+        [0, 0, 1, 1, 1],
+        [0, 0, 1, 1, 0],
+        [0, 1, 1, 0, 0]
     ])
 
     flt = np.array([
-        [1, 2, 3],
-        [1, 2, 3],
-        [1, 2, 3]
+        [1, 0, 1],
+        [0, 1, 0],
+        [1, 0, 1]
     ])
     generate_testbench(cin, flt)
+    generate_random_testbench((20, 10), (2, 3))
     return
 
 
